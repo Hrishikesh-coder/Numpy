0002249036f7ebc5d1443048a40010539ff89036
Hrishikesh <hrishipotter123@gmail.com>
2020-04-12 13:36:42 +0530

    New changes

--- First Tut.py
+++ First Tut.py
@@ -1,5 +1,20 @@
 import numpy as np
 
-a = np.array([1,2,3])
+a = np.array([1,2,3], dtype='int8')
 
 print(a)
+
+b = np.array([[9.0,8.0,7.0],[6.0,5.0,4.0]])
+
+print(b)
+
+print(b.ndim)
+
+print(b.shape)
+
+print(a.dtype)
+
+print(a.itemsize)
+
+print(a.size)
+print(a.nbytes)
